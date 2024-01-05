9f766bbe3a3c1198ac33f4d1f19f394bbc7f493b
AbEl9876 <133850497+AbEl9876@users.noreply.github.com>
2024-01-05 02:00:29 +0100

    #devel Start of the AP menu

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ import time
 import schedule
 from datetime import datetime, timedelta
 import re
+import smtplib
+import threading
+
+scheduler_running = True
 
 #Connected MAC list
 connected_macs=[]
@@ -23,22 +27,24 @@ def update_accept_MAC_list():
                 accepted_macs.append(line.strip())
     except:
         pass
-    
-    with open('time_ranges.txt') as f:
-        for line in f:
-            mac, initial_time, final_time = line.strip().split()
-            current_time = datetime.now().strftime('%H:%M')
-            if current_time >= initial_time and current_time < final_time:
-                lines_to_keep.append(mac)
-                if mac not in accepted_macs:
-                    subprocess.run(f"hostapd_cli accept_acl ADD_MAC {mac}", shell=True)
-            else:
-                if mac in accepted_macs:
-                    subprocess.run(f"hostapd_cli accept_acl DEL_MAC {mac}", shell=True)
-                    if mac in connected_macs:
-                        print(mac, "reached end time.")
-    with open('allowed_macs.txt', 'w') as f:
-        f.writelines(lines_to_keep)
+    try:
+        with open('time_ranges.txt') as f:
+            for line in f:
+                mac, initial_time, final_time = line.strip().split()
+                current_time = datetime.now().strftime('%H:%M')
+                if current_time >= initial_time and current_time < final_time:
+                    lines_to_keep.append(mac)
+                    if mac not in accepted_macs:
+                        subprocess.run(f"hostapd_cli accept_acl ADD_MAC {mac}", shell=True)
+                else:
+                    if mac in accepted_macs:
+                        subprocess.run(f"hostapd_cli accept_acl DEL_MAC {mac}", shell=True)
+                        if mac in connected_macs:
+                            print(mac, "reached end time.")
+        with open('allowed_macs.txt', 'w') as f:
+            f.writelines(lines_to_keep)
+    except:
+        pass
     
 
 def validate_mac_address(mac_address):
@@ -96,7 +102,37 @@ def monitor_log_file():
     for log_entry in output.splitlines():
         update_mac_lists(log_entry)
 
-if __name__=='__main__':
+
+def validate_email(email):
+    # Basic email format validation
+    pattern = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    return re.match(pattern, email)
+
+def send_email(correu, assumpte, text):
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login("proves908767@gmail.com", "lpxwyfipjftgfcnr")
+        message = f'Subject: {assumpte}\n\n{text}'
+        server.sendmail("proves908767@gmail.com", correu, message)
+        server.close()
+    except Exception as e:
+        pass        
+
+#We create the following thread so that scheduled jobs are executed at the right time while we have the menu working
+def background_scheduler():
+    global scheduler_running
+    while scheduler_running:
+        schedule.run_pending()
+        time.sleep(1)
+
+scheduler_thread = threading.Thread(target=background_scheduler)
+
+def main():
+    admin_email = ""
+    max_attempts = 3
+    attempts = 0
     #Wi-Fi configuration
     wifi_config = {
         'interface': 'wlp3s0',
@@ -111,17 +147,22 @@ if __name__=='__main__':
     # Generate hostapd.conf file
     generate_hostapd_conf(wifi_config)
 
-    print('#'*39,'\n Introduce accepted MAC list for your AP\n','#'*39)
-    while True:
-        generate_allowed_macs_file()
-        another_entry = input("Do you want to add a new MAC address to the accept list? (Y/N): ").strip().upper()
-        while another_entry != 'Y' and another_entry != 'N':
-            print("Invalid input. Please enter 'Y' or 'N'.")
-            another_entry = input("Do you want to add a new MAC address to the accept list? (Y/N): ").strip().upper()
-        if another_entry == 'N':
-            print("Data saved successfully.")
-            print("Initializing your AP.")
+    # Request administrator's email
+    while attempts < max_attempts:
+        admin_email = input("Enter the admin email address: ")
+
+        if validate_email(admin_email):
             break
+        else:
+            attempts += 1
+            print(f"Incorrect email format. Attempt {attempts} of {max_attempts}.\n")
+
+    if attempts == max_attempts:
+        print("Too many failed attempts. Exiting the program.")
+        return
+    
+    # Generate hostapd.conf file
+    generate_hostapd_conf(wifi_config)
 
     # Schedule the disconnect function every 1 minute
     schedule.every(10).seconds.do(update_accept_MAC_list)
@@ -137,15 +178,63 @@ if __name__=='__main__':
 
     update_accept_MAC_list()
 
+    scheduler_thread.start()
+
     try:
         while True:
-            schedule.run_pending()
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Exiting...")
+            print('#' * 20)
+            print("MENU".center(20, ' '))
+            print('#' * 20)
+            print("1. Enter accepted MAC list for your AP")
+            print("2. Show the list of devices of interest and the time interval when they can connect")
+            print("3. Devices connected at this time")
+            print("4. Receive information from the third and fourth option via email")
+            print("5. Exit")
 
+            choice = input("Select an option (1-5): ")
+
+            if choice == '1':
+                print("Option 1 selected.")
+                while True:
+                    generate_allowed_macs_file()
+                    another_entry = input("Do you want to add a new MAC address to the accept list? (Y/N): ").strip().upper()
+                    while another_entry != 'Y' and another_entry != 'N':
+                        print("Invalid input. Please enter 'Y' or 'N'.")
+                        another_entry = input("Do you want to add a new MAC address to the accept list? (Y/N): ").strip().upper()
+                    if another_entry == 'N':
+                        print("Data saved successfully.")
+                        print("Initializing your AP.")
+                        break
+            elif choice == '2':
+                # Logic for the second option
+                print("Option 2 selected.")
+            elif choice == '3':
+                # Logic for the third option
+                print("Option 3 selected.")
+            elif choice == '4':
+                # Logic for the fourth option
+                print("Option 4 selected.")
+                subject = "Information about connected devices"
+                body = "Here is the information you want to send via email."
+                send_email(admin_email, subject, body)
+                print(f"Information sent by email to {admin_email}.")
+            elif choice == '5':
+                print("Exiting the program. Goodbye!")
+                break
+            else:
+                print("Invalid option. Please select an option from 1 to 5.")
+    except:
+        print("Exiting...")
+    
     # Stop hostapd on exit
     stop_command = "pkill hostapd"
     subprocess.run(stop_command, shell=True)
+    return
+
+    
+
+
+if __name__=='__main__':
+    main()
 
 
